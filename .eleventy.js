0b7f29180e1d9b815135b8fdeb8ce4b6feb16fe2
steveostudios <steveostudios@gmail.com>
2023-01-30 15:13:53 -0500

    added ranking to bookstats

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
-const pluginNavigation = require('@11ty/eleventy-navigation')
-const pluginRss = require('@11ty/eleventy-plugin-rss')
-const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const svgContents = require("eleventy-plugin-svg-contents");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/projects/**/*.jpg");
-  eleventyConfig.addPassthroughCopy("src/blog/**/*.jpg")
+  eleventyConfig.addPassthroughCopy("src/blog/**/*.jpg");
 
   eleventyConfig.addPlugin(pluginNavigation);
   eleventyConfig.addPlugin(pluginRss);
@@ -17,8 +17,10 @@ module.exports = function (eleventyConfig) {
 
   // return a single book from the title
   eleventyConfig.addFilter("getBook", function (books, title) {
-    return books.find(book => book.title.toLowerCase() === title.toLowerCase());
-  })
+    return books.find(
+      (book) => book.title.toLowerCase() === title.toLowerCase()
+    );
+  });
 
   eleventyConfig.addCollection("projects", (collections) => {
     // get all posts by tag 'post'
@@ -34,16 +36,28 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLiquidFilter("reverse", (collection) => {
     const arr = [...collection];
     return arr.reverse();
-  })
+  });
 
   eleventyConfig.addLiquidFilter("number_with_delimiter", (num) => {
     return Number(num).toLocaleString();
   });
 
+  eleventyConfig.addLiquidFilter("to_ordinal", (num) => {
+    let ord = "th";
+    if (num % 10 == 1 && num % 100 != 11) {
+      ord = "st";
+    } else if (num % 10 == 2 && num % 100 != 12) {
+      ord = "nd";
+    } else if (num % 10 == 3 && num % 100 != 13) {
+      ord = "rd";
+    }
+    return num + ord;
+  });
+
   eleventyConfig.addLiquidFilter("max", (collection, prop) => {
-    const arr = [...collection].sort((a, b) => a[prop] > b[prop] ? 1 : -1);
+    const arr = [...collection].sort((a, b) => (a[prop] > b[prop] ? 1 : -1));
     return arr.reverse()[0];
-  })
+  });
 
   // return books by year
   eleventyConfig.addFilter("getBooksByYear", function (books) {
@@ -51,30 +65,54 @@ module.exports = function (eleventyConfig) {
     const getYears = books
       .filter((book) => book.dateFinish) // filter only finished books
       .sort((a, b) => new Date(b.dateFinish) - new Date(a.dateFinish)) // sort by finish date
-      .reduce((acc, cur) => { // add them to arrays by year
+      .reduce((acc, cur) => {
+        // add them to arrays by year
         acc[new Date(cur["dateFinish"]).getFullYear()] = [
           ...(acc[new Date(cur["dateFinish"]).getFullYear()] || []),
           cur,
         ];
         return acc;
-      }, {})
+      }, {});
 
     // get currently reading books
     // throw them in front of the read books
-    const getYearsPlusCurrent = {current: books.filter(book => book.progress), ...getYears};
+    const getYearsPlusCurrent = {
+      current: books.filter((book) => book.progress),
+      ...getYears,
+    };
 
     // pass clean data to get view
     const bookObject = Object.entries(getYearsPlusCurrent) // put into array
-    .reverse() // sort reverse (recent first)
-    .map(([year, books]) => // add meta for each year
-      ({
-        year: year,
-        items: [...books],
-        bookCount: books.length,
-        pageCount: books.reduce((acc, cur) => acc + cur.pages, 0)
-      })
-    )
+      .reverse() // sort reverse (recent first)
+      .map(
+        (
+          [year, books] // add meta for each year
+        ) => ({
+          year: year,
+          items: [...books],
+          bookCount: books.length,
+          pageCount: books.reduce((acc, cur) => acc + cur.pages, 0),
+        })
+      );
+    // console.log(bookObject);
+    const now = new Date().getFullYear();
 
+    const bookRank = bookObject.filter(
+      (year) =>
+        year.bookCount >
+        bookObject.find((item) => item.year === now.toString()).bookCount
+    );
+
+    const pageRank = bookObject.filter(
+      (year) =>
+        year.pageCount >
+        bookObject.find((item) => item.year === now.toString()).pageCount
+    );
+    // console.log(bookRank);
+    // console.log(now);
+    // console.log(bookObject.find((item) => item.year === now.toString()));
+
+    const placeString = ["th", "st", "nd", "rd", "th"];
 
     return {
       books: bookObject,
@@ -82,49 +120,96 @@ module.exports = function (eleventyConfig) {
       finishedPages: books
         .filter((book) => book.dateFinish)
         .reduce((acc, cur) => acc + cur.pages, 0),
+      bookRank: bookRank.length,
+      pageRank: pageRank.length,
     };
-  })
+  });
 
   const year = (year, value, x, y, h, position) => {
     return `
       <g class="year ${year}">
         <circle class="dot" cx="${x}" cy="${y}" r="4"/>
-        <text class="value" x="${x}" y="${y - 8}" text-anchor="${position}">${value}</text>
+        <text class="value" x="${x}" y="${
+      y - 8
+    }" text-anchor="${position}">${value}</text>
         <text class="xyear" x="${x}" y="${h + 5}">${year}</text>
       </g>
     `;
-  }
+  };
 
   const graphSVG = (data, prop, options) => {
-    const viewBox = {w: 420, h: 180}; // how big the SVG is
-    const graphDims = {x: 5, y: 20, w: 410, h: 130}; // how big the "graph" portion is in the SVG
+    const viewBox = { w: 420, h: 180 }; // how big the SVG is
+    const graphDims = { x: 5, y: 20, w: 410, h: 130 }; // how big the "graph" portion is in the SVG
     const horLinesGap = options?.gap || 5;
 
     // get rid of 'current' since they are not finished books
-    data = data.filter(item => item.year !== "current").sort((a,b) => a.year - b.year)
-    
+    data = data
+      .filter((item) => item.year !== "current")
+      .sort((a, b) => a.year - b.year);
+
     // get bounds (fills in years where I didn't read)
     const firstYear = data[0].year;
     const lastYear = data[data.length - 1].year;
     const steps = lastYear - firstYear + 1;
 
     // create array with empty dates
-    const filledInBooks = Array.from({length: steps}).map((newItem, i) => {
-      const match = data.find(item => parseInt(item.year) === i + parseInt(firstYear));
-      return match || {year: (i + parseInt(firstYear)).toString(), items: [], bookCount: 0, pageCount: 0}
-    })
+    const filledInBooks = Array.from({ length: steps }).map((newItem, i) => {
+      const match = data.find(
+        (item) => parseInt(item.year) === i + parseInt(firstYear)
+      );
+      return (
+        match || {
+          year: (i + parseInt(firstYear)).toString(),
+          items: [],
+          bookCount: 0,
+          pageCount: 0,
+        }
+      );
+    });
 
     // boundaries
-    const graphHeight = [...filledInBooks].sort((a,b) => b[prop] - a[prop])[0][prop];
-    const divisions = {h: graphDims.w / (steps - 1), v: graphDims.h / graphHeight};
+    const graphHeight = [...filledInBooks].sort((a, b) => b[prop] - a[prop])[0][
+      prop
+    ];
+    const divisions = {
+      h: graphDims.w / (steps - 1),
+      v: graphDims.h / graphHeight,
+    };
 
     // svg elements
-    const horLines = [...Array(Math.ceil(graphHeight /horLinesGap))].map((item, i) => `<line class="horizontal" x1="${graphDims.x}" x2="${graphDims.w + graphDims.x}" y1="${graphDims.h - (divisions.v * i *horLinesGap) + graphDims.y}" y2="${graphDims.h - (divisions.v * i *horLinesGap) + graphDims.y}"></line>`)
-    const points = filledInBooks.map((item, i) => [(item.year - firstYear) * divisions.h + graphDims.x, (graphHeight - item[prop]) * divisions.v + graphDims.y]);
-    const fillPath = `${points.join(" ").replace("NaN", (filledInBooks.length) * divisions.h)} ${graphDims.w + graphDims.x},${graphDims.h + graphDims.y} 0, ${graphDims.h + graphDims.y}`;
-    const linePath = points.join(" ").replace("NaN", (filledInBooks.length) * divisions.h)
+    const horLines = [...Array(Math.ceil(graphHeight / horLinesGap))].map(
+      (item, i) =>
+        `<line class="horizontal" x1="${graphDims.x}" x2="${
+          graphDims.w + graphDims.x
+        }" y1="${
+          graphDims.h - divisions.v * i * horLinesGap + graphDims.y
+        }" y2="${
+          graphDims.h - divisions.v * i * horLinesGap + graphDims.y
+        }"></line>`
+    );
+    const points = filledInBooks.map((item, i) => [
+      (item.year - firstYear) * divisions.h + graphDims.x,
+      (graphHeight - item[prop]) * divisions.v + graphDims.y,
+    ]);
+    const fillPath = `${points
+      .join(" ")
+      .replace("NaN", filledInBooks.length * divisions.h)} ${
+      graphDims.w + graphDims.x
+    },${graphDims.h + graphDims.y} 0, ${graphDims.h + graphDims.y}`;
+    const linePath = points
+      .join(" ")
+      .replace("NaN", filledInBooks.length * divisions.h);
 
-    const years = points.map((item, i) => year(filledInBooks[i].year, filledInBooks[i][prop], item[0], item[1], graphDims.h + graphDims.y, i === 0 ? "start" : i === filledInBooks.length - 1 ? "end" : "middle"))
+    const years = points.map((item, i) =>
+      year(
+        filledInBooks[i].year,
+        filledInBooks[i][prop],
+        item[0],
+        item[1],
+        graphDims.h + graphDims.y,
+        i === 0 ? "start" : i === filledInBooks.length - 1 ? "end" : "middle"
+      )
+    );
 
     return `<svg viewBox="0 0 ${viewBox.w} ${viewBox.h}" width="100%" height="${viewBox.h}">
     ${horLines}
@@ -133,83 +218,90 @@ module.exports = function (eleventyConfig) {
     ${years}
 
     </svg>`;
-  }
+  };
 
   eleventyConfig.addLiquidShortcode("booksByYearSVG", (books) => {
-    return graphSVG(books, 'bookCount')
-  })
+    return graphSVG(books, "bookCount");
+  });
 
   eleventyConfig.addLiquidShortcode("pagesByYearSVG", (books) => {
-    return graphSVG(books, 'pageCount', {gap: 1000});
-  })
+    return graphSVG(books, "pageCount", { gap: 1000 });
+  });
 
   const pieChart = (data) => {
-    const viewBox = {w: 420, h: 180}; // how big the SVG is
+    const viewBox = { w: 420, h: 180 }; // how big the SVG is
 
-    let colors = [ '#0f688a', '#89cae2', '#67afc9', '#dedcd6' ];
+    let colors = ["#0f688a", "#89cae2", "#67afc9", "#dedcd6"];
     const total = data.reduce((prev, curr) => {
-      return prev + curr.value
+      return prev + curr.value;
     }, 0);
 
-    const slices = data.map((item, i ) => {return {
-      color: colors[i],
-      value: item.value,
-      title: item.title,
-      percent: item.value / total
-    }})
+    const slices = data.map((item, i) => {
+      return {
+        color: colors[i],
+        value: item.value,
+        title: item.title,
+        percent: item.value / total,
+      };
+    });
 
     let cumulativePercent = 0;
-    
+
     function getCoordinatesForPercent(percent) {
       const x = Math.cos(2 * Math.PI * percent);
       const y = Math.sin(2 * Math.PI * percent);
       return [x, y];
     }
-    
+
     const pieces = slices.map((slice, i) => {
       // destructuring assignment sets the two variables at once
       const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
-      
+
       // each slice starts where the last slice ended, so keep a cumulative percent
       cumulativePercent += slice.percent;
-      
+
       const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
-    
+
       // if the slice is more than 50%, take the large arc (the long way around)
-      const largeArcFlag = slice.percent > .5 ? 1 : 0;
-    
+      const largeArcFlag = slice.percent > 0.5 ? 1 : 0;
+
       // create an array and join it just for code readability
       pathData = [
         `M ${startX} ${startY}`, // Move
         `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
         `L 0 0`, // Line
-      ].join(' ');
+      ].join(" ");
 
-      return `<path d="${pathData}" fill="${colors[i]}"></path>`
-    })
+      return `<path d="${pathData}" fill="${colors[i]}"></path>`;
+    });
 
-    const legend = slices.map((item, i) => {
-      return `<g>
-        <rect x="1.4" y="${-1 + (i * 0.25)}" width="0.2" height="0.2" fill="${item.color}" />
-        <text x="1.65" y="${-0.8125 + (i * 0.25)}" style="font-size: 0.2px;">${item.title}</text>
+    const legend = slices
+      .map((item, i) => {
+        return `<g>
+        <rect x="1.4" y="${-1 + i * 0.25}" width="0.2" height="0.2" fill="${
+          item.color
+        }" />
+        <text x="1.65" y="${-0.8125 + i * 0.25}" style="font-size: 0.2px;">${
+          item.title
+        }</text>
       </g>`;
-    }).join("")
-
+      })
+      .join("");
 
     return `<svg height="${viewBox.h}" width="${viewBox.w}" viewBox="0 -1 2 2" style="border:1px solid gray; ">
       ${pieces}
       ${legend}
-    </svg>`
-  }
+    </svg>`;
+  };
 
   eleventyConfig.addLiquidShortcode("fictionPieChart", () => {
     return pieChart([
-      {title: "Fiction", value: 10},
-      {title: "-pseudoFiction", value: 20},
-      {title: "Non-Fiction", value: 10},
-      {title: "Non-Fiction", value: 10}
-    ])
-  })
+      { title: "Fiction", value: 10 },
+      { title: "-pseudoFiction", value: 20 },
+      { title: "Non-Fiction", value: 10 },
+      { title: "Non-Fiction", value: 10 },
+    ]);
+  });
 
   return {
     passthroughFileCopy: true,
